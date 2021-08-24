daf3c4ed39a817d905ed6f1014996d59578a50b0
AJ Van Beest <ajvanbeest@protonmail.com>
2021-08-24 18:41:58 -0500

    Added PW audit folder

---
+++ Auditing_AD_Passwords/DumbADPasswordChecker.ps1
@@ -0,0 +1,9 @@
+<#
+Name:           DumbADPasswordChecker
+Author:         AJ Van Beest
+Last modified:  20210824T1834
+
+Description:
+        Audit your active directory passwords for weak passwords, ask people with weak passwords to change them, force the change when they don't, and build statistics about these efforts to report out.
+
+#>
