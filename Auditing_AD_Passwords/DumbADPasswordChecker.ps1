23cadf0914a71f4a10acb6ae93e44115d158b86e
A.J. Van Beest <AVanBeest@Paylocity.com>
2021-08-25 11:53:50 -0500

    Updated .ps1

--- Auditing_AD_Passwords/DumbADPasswordChecker.ps1
+++ Auditing_AD_Passwords/DumbADPasswordChecker.ps1
@@ -9,19 +9,41 @@ Description:
 #>
 
 <# ======== Set Variables ======== #>
-$HashcatURI = "https://hashcat.net/files/hashcat-6.2.3.7z"
-$HashcatFile = ".\"
-$HashcatHash = "Invoke-WebRequest -URI https://hashcat.net/files/hashcat-6.2.3.7z.asc"
-
+$WorkingDir = "C:\users\avanbeest\testing\"
 
 <# ======== Handle credentials ======== #>
 
 
-<# ======== Install tools ======== #>
+<# ======== Download tools ======== #>
+        <#
+                TO-DO:
+                - Checks to see if tools exist
+                - Offer to install tools automatically
+        #>
+
+        # Build directory structure
+        $Installers = $WorkingDir + "installers"; mkdir $Installers
+        $Dir7Zip = $Installers + "\7Zip"; mkdir $Dir7Zip
+        $DirHashcat = $Installers + "\hashcat"; mkdir $DirHashcat
+        $DirMentalist = $Installers + "\mentalist"; mkdir $DirMentalist
+        
+        #7zip
+                # TO-DO: Check to see if 7zip is installed
+        $Installer7Zip = $Dir7Zip+ "installers\7z1900-x64.msi"; 
+        Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile $Installer7Zip
+        
         # Hashcat
-        # >> Check for hashcat installation; If not installed, do it.
-        Invoke-WebRequest -URI $HashcatURI -OutFile $HashcatFile
-        # >> Check the hash and install
+        $InstallerHashcat = $DirHashcat + "\hashcat-6.2.3.7z"
+        $HashHashcat = $DirHashcat + "\hashcat-6.2.3.7z.asc"
+        Invoke-WebRequest "https://hashcat.net/files/hashcat-6.2.3.7z" -OutFile $InstallerHashcat
+        Invoke-WebRequest "https://hashcat.net/files/hashcat-6.2.3.7z.asc" -OutFile $HashHashcat
+        Expand-7Zip $InstallerHashcat $DirHashcat
+
+        #Mentalist
+        $InstallerMentalist = $DirMentalist + "\master.zip"
+        Invoke-WebRequest "https://github.com/sc0tfree/mentalist/archive/refs/heads/master.zip" -OutFile $InstallerMentalist
+        Expand-Archive $InstallerMentalist $DirMentalist
+        
 
         #Python
         # >> Check for python installation; if not installed, do it.
