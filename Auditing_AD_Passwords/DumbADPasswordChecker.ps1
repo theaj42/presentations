318d3ef64d4054980cd6151c144c86d164219262
AJ Van Beest <ajvanbeest@protonmail.com>
2021-08-24 21:42:39 -0500

    Updated ps1

--- Auditing_AD_Passwords/DumbADPasswordChecker.ps1
+++ Auditing_AD_Passwords/DumbADPasswordChecker.ps1
@@ -9,19 +9,35 @@ Description:
 #>
 
 <# ======== Set Variables ======== #>
+$HashcatURI = "https://hashcat.net/files/hashcat-6.2.3.7z"
+$HashcatFile = ".\"
+$HashcatHash = "Invoke-WebRequest -URI https://hashcat.net/files/hashcat-6.2.3.7z.asc"
 
 
 <# ======== Handle credentials ======== #>
 
 
-<# ======== Build a "Dictionary of Dumb" ======== #>
+<# ======== Install tools ======== #>
+        # Hashcat
+        # >> Check for hashcat installation; If not installed, do it.
+        Invoke-WebRequest -URI $HashcatURI -OutFile $HashcatFile
+        # >> Check the hash and install
 
+        #Python
+        # >> Check for python installation; if not installed, do it.
+
+        #Mentalist
+        # >> Check for mentalist; if not installed do it.
+
+
+<# ======== Build a "Dictionary of Dumb" ======== #>
+        # Run mentalist to build a wordlist
 
 <# ======== Dump you some hashes ======== #>
 
 
 <# ======== Crack you some hashes ======== #>
-
+        # Run Hashcat against the hashes using the Mentalist wordlist
 
 <# ======== Send nice emails ======== #>
 
