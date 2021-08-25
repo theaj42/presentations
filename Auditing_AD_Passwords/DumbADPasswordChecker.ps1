fb4f5032d8b4947a263b6386a240eb9b2e53269e
AJ Van Beest <ajvanbeest@protonmail.com>
2021-08-24 20:24:18 -0500

    Updated ps1

--- Auditing_AD_Passwords/DumbADPasswordChecker.ps1
+++ Auditing_AD_Passwords/DumbADPasswordChecker.ps1
@@ -7,3 +7,32 @@ Description:
         Audit your active directory passwords for weak passwords, ask people with weak passwords to change them, force the change when they don't, and build statistics about these efforts to report out.
 
 #>
+
+<# ======== Set Variables ======== #>
+
+
+<# ======== Handle credentials ======== #>
+
+
+<# ======== Build a "Dictionary of Dumb" ======== #>
+
+
+<# ======== Dump you some hashes ======== #>
+
+
+<# ======== Crack you some hashes ======== #>
+
+
+<# ======== Send nice emails ======== #>
+
+
+<# ======== Initial reporting ======== #>
+
+
+<# ======== Send "Now it's gettin' real emais" ======== #>
+
+
+<# ======== Send sorry/not-sorry emails ======== #>
+
+
+<# ======== Final reporting ======== #>
